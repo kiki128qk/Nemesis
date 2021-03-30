75179536c4296eb0ffbb9d970fcd92f247d9ba0c
jung_jung <kiki128qk@naver.com>
2021-03-30 11:16:18 +0900

    03-30 헤더 완성 신상품 슬라이더 구현중

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NEMESIS</title>
+  <!-- reset css -->
+  <link rel="stylesheet" href="/nemesis/css/reset.css">
+  <!-- fontawesome link -->
+  <script src="https://kit.fontawesome.com/9e31582c84.js" crossorigin="anonymous"></script>
+  <!-- style css link -->
+  <link rel="stylesheet" href="/nemesis/css/style.css"> 
+  <!-- media css link -->
+  <link rel="stylesheet" href="/nemesis/css/media.css">
+  
+  <!-- favicon link -->
+  <link rel="icon" href="/nemesis/data/img/favicon.ico" />
+
+</head>
+<body>
+  <div class="wrap ">
+    <header class="center">
+      <div class="header_box">
+        <div class="rogo">
+          <img src="/nemesis/data/img/rogo.png" alt="">
+        </div>
+        <!-- <div class="member_menu"> -->
+          <div class="member_menu">
+            <input type="text" class="id">
+            <input type="text" class="pass">
+            <button type="button" class="button">Login</button>
+            <button type="button" class="button">Join</button>
+          </div>
+        <!-- </div> -->
+      </div>
+    </header>
+    <hr>
+    <div class="nav center">
+      <ul>
+        <li class="one_depht">
+          <a href="#">INFO</a>
+        </li>
+        <li class="one_depht">
+          <a href="#">PEREFUME</a>
+          <!-- <ul class="two_depht">Perfume</ul>
+          <ul class="two_depht">DIY Kit</ul> -->
+        </li>
+        <li class="one_depht">
+          <a href="#">CUTORM</a>
+        </li>
+        <li class="one_depht">
+          <a href="#">Q&A</a>
+        </li>
+      </ul>
+      <div class="member_icon">
+        <a href="#"><img src="/nemesis/data/icon/shopping_bag.svg" alt=""></a>
+        <a href="#"><img src="/nemesis/data/icon/shopping_cart.svg" alt=""></a>
+        <a href="#"><img src="/nemesis/data/icon/add_user.svg" alt=""></a>
+      </div>
+    </div>
+    <hr>
+  </div>
+  <div class="sliber_box">
+  <img src="https://via.placeholder.com/1000x300"/>
+
+  </div>
+
+  <section class="new_box center">
+    <h1>NEW</h1>
+    <span><i class="far fa-plus-square"></i></span>
+    <ul>
+      <li><a href="#"><img src="https://via.placeholder.com/150x200" alt=""></a></li>
+      <li><a href="#"><img src="https://via.placeholder.com/150x200" alt=""></a></li>
+      <li><a href="#"><img src="https://via.placeholder.com/150x200" alt=""></a></li>
+      <li><a href="#"><img src="https://via.placeholder.com/150x200" alt=""></a></li>
+      <li><a href="#"><img src="https://via.placeholder.com/150x200" alt=""></a></li>
+      <li><a href="#"><img src="https://via.placeholder.com/150x200" alt=""></a></li>
+    </ul>
+  </section>
+
+  <!-- jquery link -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</body>
+</html>
